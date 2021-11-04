f71903a7d58130e4210e77d58f65e003a14a1a37
sanprabe <sanprabet@gmail.com>
2021-11-04 10:54:08 -0500

    Se crearon las funciones para hallar area y perimetro

---
+++ figuras.js
@@ -0,0 +1,19 @@
+function pCuadrado(lado){
+    return lado * 4
+}
+function aCuadrado(lado){
+    return lado**2
+}
+function pTriangulo(lado1, lado2, lado3){
+    return lado1 + lado2 + lado3
+}
+function aTriangulo(base, altura){
+    return (base*altura)/2
+}
+function pCirculo(radius){
+    return 2*(Math.PI)*(radius)
+}
+function aCirculo(){
+    return (Math.PI)*(radius**2)
+}
+
