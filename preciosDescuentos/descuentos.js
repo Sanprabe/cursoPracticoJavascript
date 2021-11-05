8580cae7c805b048ffb7ae419d842c8ea2ac9a6c
sanprabe <sanprabet@gmail.com>
2021-11-04 22:31:18 -0500

    Se creo la base del HTML, se crearon funciones y se conectó el html con ids

--- preciosDescuentos/descuentos.js
+++ preciosDescuentos/descuentos.js
@@ -1,1 +1,15 @@
-console.log("lol")
+function precioFinal(precioInicial, descuento){
+    return (( (100 - descuento) * precioInicial ) / 100);
+}
+
+function onclickPrecioFinal(){
+    const inputPrecioInicial = document.getElementById("inputPrecioIncial");
+    const inputDescuento = document.getElementById("inputDescuento");
+
+    const precioInicial = inputPrecioInicial.value;
+    const descuento = inputDescuento.value;
+
+    const outputPrecioFinal = precioFinal(precioInicial, descuento);
+
+    console.log(outputPrecioFinal);
+}
