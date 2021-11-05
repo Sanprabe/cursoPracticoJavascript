f1bdf50052ca935229a1e17285b4ef1439c520a4
sanprabe <sanprabet@gmail.com>
2021-11-04 23:57:46 -0500

    Se creo el sistema de descuento por input además del sistema de cupones. Segundo proyecto finalizado

--- preciosDescuentos/descuentos.js
+++ preciosDescuentos/descuentos.js
@@ -1,15 +1,46 @@
-function precioFinal(precioInicial, descuento){
-    return (( (100 - descuento) * precioInicial ) / 100);
+function calcularPrecioFinal(precioInicial, descuentoIncial, cuponCliente){
+    
+    let descuentoCupon = 0;
+
+    if(!(precioInicial > 0 && descuentoIncial >= 0)){
+        alert("No se puede calcular con estos valores");
+        return;
+    }
+
+    if (cuponCliente != ""){
+        const cupon = coupones.find((cuponGeneral) => {return cuponGeneral.name === cuponCliente});
+        if (typeof(cupon) === undefined){
+            alert("Cupon no valido");
+        } else{
+            descuentoCupon = cupon.discount;
+        }
+    }
+
+    
+
+    const precioConDescuentoInicial = (( (100 - descuentoIncial) * precioInicial ) / 100);
+
+    const precioFinalConAmbosDescuentos = (( (100 - descuentoCupon) * precioConDescuentoInicial ) / 100);
+
+    return precioFinalConAmbosDescuentos;
 }
 
 function onclickPrecioFinal(){
     const inputPrecioInicial = document.getElementById("inputPrecioIncial");
     const inputDescuento = document.getElementById("inputDescuento");
+    const inputCupon = document.getElementById("inputCupon");
 
-    const precioInicial = inputPrecioInicial.value;
-    const descuento = inputDescuento.value;
+    const precioInicial = parseInt(inputPrecioInicial.value);
+    const descuento = parseInt(inputDescuento.value);
+    const cupon = inputCupon.value.toUpperCase();
 
-    const outputPrecioFinal = precioFinal(precioInicial, descuento);
 
-    console.log(outputPrecioFinal);
+    const precioFinal = calcularPrecioFinal(precioInicial, descuento, cupon);
+
+    const elementOutputPrecioFinal = document.getElementById("outputResult");
+
+    elementOutputPrecioFinal.innerText = `El precio final con descuento es de $${precioFinal}`
 }
+
+
+coupones = [{name:"HI", discount: 25}, {name:"BYE", discount: 25}, {name:"LOL", discount: 25}]
