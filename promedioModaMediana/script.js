aa0aedeae066daa0c7b06df0565bb29b8ea8e346
sanprabe <sanprabet@gmail.com>
2021-11-06 08:26:18 -0500

    Se hizo debug de funcion promedio y media. Se creó y se testeó la funcion de moda.

--- promedioModaMediana/script.js
+++ promedioModaMediana/script.js
@@ -1,27 +1,63 @@
-const arrEx = [100, 340, 230, 580, 32, 12]; // Definir valores
-arrEx = arrEx.sort((a,b) => {return a-b}); // Organizar valores
 
 function calcularPromedio(arr){
     let sumArr = 0;
-    for (let num in arr){
-        sumArr = sumArr + num
+    let sizeArr = arr.length;
+    for (let i = 0; i < sizeArr; i++){
+        sumArr = sumArr + arr[i];
     }
-
-    return sumArr / arr.lenght
+    return sumArr / sizeArr;
 }
 
 function calcularModa(arr){
+    let nums = [];
+    let counts = [];
+    let currNum = arr[0];
+    let currCount = 0;
+    
+    for(let i = 0; i < arr.length; i++){
+        currCount++
+        if(currNum != arr[i+1]){
+            nums.push(currNum);
+            counts.push(currCount);
+            currNum = arr[i+1];
+            currCount = 0;
+        }
+    }
+
+    let maxCounts = Math.max(...counts);
+    
+    for(let j = 0; j < nums.length; j++){
+        
+        if(counts[j] != maxCounts){
+            counts.splice(j, 1);
+            nums.splice(j, 1);
+            j--
+        }
+    }
+    
+    if(nums.length != 1){
+        return "La moda no existe en este array";
+    }else{
+        return nums[0];
+    }
 
 }
 
 function calcularMediana(arr){
-    let medianaIndex = arr.lenght;
-
-    if (medianaIndex != Math.floor(medianaIndex)){
-        arrayMediana = [Math.floor(medianaIndex), Math.ceil(medianaIndex)];
-        return calcularPromedio(arrayMediana);
+    let medio = arr.length;
+    
+    if (medio % 2 == 0){
+        let arrMediana = [arr[(medio/2)-1], arr[medio/2]];
+        return calcularPromedio(arrMediana);
     }else{
-        return medianaIndex;
+        return arr[(medio/2)-0.5];
     }
 }
-    
+
+// let arrEx = [100, 340, 230, 580, 32, 12, 13]; // Definir valores
+let arrEx = [0, 1, 1, 1, 1, 2, 3, 3, 4, 4, 4, 4, 5, 6, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9];
+arrEx = arrEx.sort((a,b) => {return a-b}); // Organizar valores
+
+console.log(`El promedio de aray ejemplo es ${calcularPromedio(arrEx)}`);
+console.log(`La moda de aray ejemplo es ${calcularModa(arrEx)}`);
+console.log(`La mediana de aray ejemplo es ${calcularMediana(arrEx)}`);
